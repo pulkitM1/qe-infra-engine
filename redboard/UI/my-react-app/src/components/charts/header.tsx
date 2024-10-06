08f788b9cbaf9daa59efe8626fcdd8a7d1f22b1e
pulkitM1 <pulkit.matta@couchbase.com>
2024-10-06 13:14:02 +0530

    reducing subheader size a bit

--- redboard/UI/my-react-app/src/components/charts/header.tsx
+++ redboard/UI/my-react-app/src/components/charts/header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
       <div className="logo-container">
         <img src={couchbaseLogo} alt="Couchbase logo" className="logo" />
         <div className="text-container">
-        <h2 className="subtitle" style={{ fontSize: "24px" }}>Couchbase</h2>
+        <h2 className="subtitle" style={{ fontSize: "18px" }}>Couchbase</h2>
         <h1 className="title" style={{ fontSize: "36px" }}>Red-Board</h1>
         </div>
       </div>
